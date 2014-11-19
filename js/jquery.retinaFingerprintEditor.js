eddfa0dbf0526b98bc3edcd33199de1ac69748ef
Taylor Peer <taylorpeer@Taylors-MacBook-Pro.local>
2014-11-19 11:38:04 +0100

    Added basic structure of Retina Fingerprint Editor plugin

--- js/jquery.retinaFingerprintEditor.js
+++ js/jquery.retinaFingerprintEditor.js
@@ -1,3 +1,35 @@
-/**
- * Created by taylorpeer on 19/11/14.
- */
+(function ($) {
+
+    $.fn.fingerprintEditor = function (options) {
+
+        /**
+         * Create a fingerprint editor out of a DOM element
+         * @param $element
+         * @param options
+         */
+        function createFingerprintEditor($element, options) {
+            // Add all default options
+            options = $.extend({}, $.fn.expressionBuilder.defaults, options);
+        }
+
+        return this.each(function (options) {
+
+            // Add all default options
+            options = $.extend({}, $.fn.expressionBuilder.defaults, options);
+
+            if (!$(this).is("div")) {
+                throw "fingerprintEditor is only applicable to DIV elements";
+            } else {
+                createFingerprintEditor($(this), options);
+            }
+        });
+    };
+
+    /**
+     * Collection of default plugin options
+     */
+    $.fn.fingerprintEditor.defaults = {
+
+    };
+
+})(jQuery);
