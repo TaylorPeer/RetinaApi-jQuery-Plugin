762d95b911dbb937be614413b0d09bd271b3b113
Taylor Peer <taylorpeer@Taylors-MacBook-Pro.local>
2014-11-25 17:00:22 +0100

    Added unit tests for text module

--- tests/tests.js
+++ tests/tests.js
@@ -16,12 +16,6 @@ var testRetinaName;
 var testTerm = "test";
 
 /**
- * Valid context IDs for the testTerm
- * @type {Array}
- */
-var testTermContextIds = [];
-
-/**
  * Term used as a parameter in wildcard tests using asterisk
  * @type {string}
  */
@@ -34,6 +28,13 @@ var testTermAsterisk = "the*";
 var testTermQuestionMark = "the?";
 
 /**
+ * Text used as a parameter in various tests
+ * @type {string}
+ */
+var testTexts = [
+    "The National Aeronautics and Space Administration (NASA) is the United States government agency that is responsible for the civilian space program as well as for aeronautics and aerospace research. President Dwight D. Eisenhower established the National Aeronautics and Space Administration (NASA) in 1958 with a distinctly civilian (rather than military) orientation encouraging peaceful applications in space science. The National Aeronautics and Space Act was passed on July 29, 1958, disestablishing NASA's predecessor, the National Advisory Committee for Aeronautics (NACA). The new agency became operational on October 1, 1958. Since that time, most U.S. space exploration efforts have been led by NASA, including the Apollo moon-landing missions, the Skylab space station, and later the Space Shuttle. Currently, NASA is supporting the International Space Station and is overseeing the development of the Orion Multi-Purpose Crew Vehicle, the Space Launch System and Commercial Crew vehicles.", "Hong Kong, officially known as Hong Kong Special Administrative Region of the People's Republic of China, is a region on the southern coast of China geographically enclosed by the Pearl River Delta and South China Sea. Hong Kong is known for its expansive skyline and deep natural harbour, and with a land mass of 1,104 km2 (426 sq mi) and a population of over seven million people, is one of the most densely populated areas in the world. Hong Kong became a colony of the British Empire after the First Opium War (1839-42). Hong Kong Island was first ceded to Great Britain in perpetuity, followed by Kowloon Peninsula in 1860 and then the New Territories was put under lease in 1898. It was occupied by Japan during The Second World War (1941-45), after which the British resumed control until 1997.", "Stephen William Hawking (born 8 January 1942) is an English theoretical physicist, cosmologist, author and Director of Research at the Centre for Theoretical Cosmology within the University of Cambridge. Among his significant scientific works have been a collaboration with Roger Penrose on gravitational singularity theorems in the framework of general relativity, and the theoretical prediction that black holes emit radiation, often called Hawking radiation. Hawking was the first to set forth a cosmology explained by a union of the general theory of relativity and quantum mechanics. He is a vocal supporter of the many-worlds interpretation of quantum mechanics. Hawking has achieved success with works of popular science in which he discusses his own theories and cosmology in general; his A Brief History of Time stayed on the British Sunday Times best-sellers list for a record-breaking 237 weeks.", "The Battle of Wolf 359 is a fictional space battle in the Star Trek universe between the United Federation of Planets and the Borg Collective in the year 2367. The aftermath is depicted in the Star Trek: The Next Generation episode \"The Best of Both Worlds, Part II\" and the battle in Star Trek: Deep Space Nine pilot, \"Emissary.\" The battle occurs at the star Wolf 359, a real star system located 7.78 light years from Earth's solar system, and constitutes a total loss for the Federation, after the single attacking Borg ship obliterates the opposing fleet and proceeds to Earth without significant damage.", "Computational biology involves the development and application of data-analytical and theoretical methods, mathematical modeling and computational simulation techniques to the study of biological, behavioral, and social systems. The field is broadly defined and includes foundations in computer science, applied mathematics, animation, statistics, biochemistry, chemistry, biophysics, molecular biology, genetics, genomics, ecology, evolution, anatomy, neuroscience, and visualization."
+];
+/**
  * Collection of setup functions to perform before unit test execution
  */
 var setup = {
@@ -42,7 +43,7 @@ var setup = {
      * Collection of setup tasks and flags indicating their state of completion
      */
     progress: {
-        setTestRetina: false,
+        setTestRetina: false
     },
 
     isSetupComplete: function () {
@@ -284,7 +285,6 @@ function runTests() {
      */
     QUnit.asyncTest("testGetSimilarTermsWithPosFilter", function (assert) {
         var posTypeFilters = ["NOUN", "VERB", "ADJECTIVE"];
-
         $.each(posTypeFilters, function (index, posTypeFilter) {
             QUnit.stop();
             var options = {retinaName: testRetinaName, term: testTerm, posType: posTypeFilter};
@@ -308,6 +308,127 @@ function runTests() {
             $.retinaApi.terms.getSimilarTerms(options, callback);
         });
         QUnit.start();
+    });
+
+    /**
+     * Tests that getRepresentationForText throws an error if no options were configured
+     */
+    QUnit.asyncTest("testGetRepresentationWithoutOptions", function (assert) {
+        assert.expect(1);
+        assert.throws(function () {
+            $.retinaApi.text.getRepresentationForText({}, $.noop);
+        }, "Call to 'getRepresentationForText' is missing the following required parameters: options.retinaName, options.text", "Exception thrown due to missing options");
+        QUnit.start();
+    });
+
+    /**
+     * Tests that getSimilarTerms returns related keywords for a given term
+     */
+    QUnit.asyncTest("testGetRepresentationForValidText", function (assert) {
+        assert.expect(2);
+        var options = {retinaName: testRetinaName, text: testTexts[0]};
+        var callback = function (data) {
+            assert.ok(data.length == 1, "Data was returned");
+            assert.notEqual(typeof data[0].positions, "undefined", "Results contain a fingerprint");
+            QUnit.start();
+        };
+        $.retinaApi.text.getRepresentationForText(options, callback);
+    });
+
+    /**
+     * Tests that getKeywordsForText throws an error if no options were configured
+     */
+    QUnit.asyncTest("testGetKeywordsForTextWithoutOptions", function (assert) {
+        assert.expect(1);
+        assert.throws(function () {
+            $.retinaApi.text.getKeywordsForText({}, $.noop);
+        }, "Call to 'getKeywordsForText' is missing the following required parameters: options.retinaName, options.text", "Exception thrown due to missing options");
+        QUnit.start();
+    });
+
+    /**
+     * Tests that getKeywordsForText returns related keywords for a given text
+     */
+    QUnit.asyncTest("testGetKeywordsForTextForValidText", function (assert) {
+        assert.expect(1);
+        var options = {retinaName: testRetinaName, text: testTexts[0]};
+        var callback = function (data) {
+            assert.ok(data.length > 0, "Keywords were returned");
+            QUnit.start();
+        };
+        $.retinaApi.text.getKeywordsForText(options, callback);
+    });
+
+    /**
+     * Tests that getTokensForText throws an error if no options were configured
+     */
+    QUnit.asyncTest("testGetTokensForTextWithoutOptions", function (assert) {
+        assert.expect(1);
+        assert.throws(function () {
+            $.retinaApi.text.getTokensForText({}, $.noop);
+        }, "Call to 'getTokensForText' is missing the following required parameters: options.retinaName, options.text", "Exception thrown due to missing options");
+        QUnit.start();
+    });
+
+    /**
+     * Tests that getTokensForText returns tokens for a given text
+     */
+    QUnit.asyncTest("testGetTokensForTextForValidText", function (assert) {
+        assert.expect(1);
+        var options = {retinaName: testRetinaName, text: testTexts[0]};
+        var callback = function (data) {
+            assert.ok(data.length > 0, "Tokens were returned");
+            QUnit.start();
+        };
+        $.retinaApi.text.getTokensForText(options, callback);
+    });
+
+    /**
+     * Tests that getTokensForText returns tokens for a given text using a POS tag filter
+     */
+    QUnit.asyncTest("testGetTokensForTextForValidText", function (assert) {
+        assert.expect(3);
+        var options = {retinaName: testRetinaName, text: testTexts[0]};
+        var callback = function (data) {
+            var unfilteredCount = JSON.stringify(data).match(/,/g).length;
+            assert.ok(unfilteredCount > 0, "Tokens were returned");
+
+            options.posTags = "NN";
+            var filteredCallback = function (data) {
+                var filteredCount = JSON.stringify(data).match(/,/g).length;
+                assert.ok(filteredCount > 0, "Tokens were returned");
+                assert.ok(unfilteredCount > filteredCount, "Filtered token count was less than the unfiltered token count");
+                QUnit.start();
+            };
+            $.retinaApi.text.getTokensForText(options, filteredCallback);
+        };
+        $.retinaApi.text.getTokensForText(options, callback);
+    });
+
+    /**
+     * Tests that getRepresentationsForBulkText throws an error if no options were configured
+     */
+    QUnit.asyncTest("testGetRepresentationsForBulkTextWithoutOptions", function (assert) {
+        assert.expect(1);
+        assert.throws(function () {
+            $.retinaApi.text.getRepresentationsForBulkText({}, $.noop);
+        }, "Call to 'getRepresentationsForBulkText' is missing the following required parameters: options.retinaName, options.texts", "Exception thrown due to missing options");
+        QUnit.start();
+    });
+
+    /**
+     * Tests that getRepresentationsForBulkText returns fingerprints for an array of texts
+     */
+    QUnit.asyncTest("testGetRepresentationsForValidBulk", function (assert) {
+        var options = {retinaName: testRetinaName, texts: testTexts};
+        var callback = function (data) {
+            assert.ok(data.length > 0, "Data was returned");
+            $.each(data, function (index, entry) {
+                assert.notEqual(typeof entry.positions, "undefined", "Returned data contained a fingerprint");
+            });
+            QUnit.start();
+        };
+        $.retinaApi.text.getRepresentationsForBulkText(options, callback);
     });
 
 }
